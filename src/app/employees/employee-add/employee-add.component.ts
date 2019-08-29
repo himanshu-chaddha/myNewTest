d5e7ea8a2ba24c7b50ae14586f7dd165a44603e1
chaddha1 <himanshuchaddha4u@gmail.com>
2019-08-30 00:14:30 +0530

    Second Commit

--- src/app/employees/employee-add/employee-add.component.ts
+++ src/app/employees/employee-add/employee-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl } from "@angular/forms";
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: "app-employee-add",
   templateUrl: "./employee-add.component.html",
@@ -12,7 +13,7 @@ export class EmployeeAddComponent implements OnInit {
   public imagePath;
   imgURL: any = "";
   public message: string;
-  constructor() {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.newEmployeeForm = new FormGroup({
@@ -31,6 +32,9 @@ export class EmployeeAddComponent implements OnInit {
   }
   onAddNewEmployee() {
     console.log(this.newEmployeeForm);
+   alert(" Added Successfully")
+    this.router.navigate(['/dashboard'])
+
   }
 
   preview(files) {
